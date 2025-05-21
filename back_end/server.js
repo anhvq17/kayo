a732ec152d4dc18668d44a50ab6fba25c193522c
ducthaii <ducthai265@gmail.com>
2025-05-21 22:21:49 +0700

    viết api cho sản phẩm, danh mục, thương hiệu

--- back_end/server.js
+++ back_end/server.js
@@ -1,10 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import connectMongoDB from "./config/db";
+import productRouter from "./routes/productRoutes";
+import categoryRouter from "./routes/categoryRoutes";
+import brandRouter from "./routes/brandRoutes";
 const app = express();
 app.use(express.json());
+app.get('/', (req, res) => {
+    console.log('Home');
+    res.send('Hello from Home');
+});
+// routers
+app.use('/products',productRouter)
+app.use('/brands',brandRouter)
+app.use('/categories',categoryRouter)
+
 // Kết nối db
 // mongoose.connect(``);
+connectMongoDB('mongodb://127.0.0.1:27017/DATN')
 
-// routers
+
 
 export const viteNodeApp = app;
